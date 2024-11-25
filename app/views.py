d36f0a8d1638bdc282a0d26d5a6bae4902ee18a1
jyotikumari36 <jyotikumari11745@gmail.com>
2024-11-25 19:16:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def jyoti(request):
+    return HttpResponse('I am python full stack developers')
